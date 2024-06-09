83e151be3ec47367e2bb05623f0ec9c089199cc5
AmjadRA <amjad.rehman@singletoncommerce.com>
2024-06-09 19:07:00 +0530

    project structure improved

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import { Queue, Worker } from 'bullmq';
 import express from 'express';
+import IORedis from 'ioredis';
 import { redisConnection } from './config.mjs';
 import bodyParser from 'body-parser';
 const app = express()
@@ -7,11 +8,20 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const connection = new IORedis({
+  maxRetriesPerRequest: null,
+});
+
 // Create a new connection in every instance
 const jobQueue = new Queue('jobQueue', { redisConnection });
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.get('/:data', (req, res) => {
+  jobQueue.add('myJobName', { foo: req.params }, { delay: 10000 });
+  res.status(200).send(req.params)
+})
 
 app.post('/add-job', async (req, res) => {
   console.log(req.body);
